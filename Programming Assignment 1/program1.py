0a51eb16720ea03e3cf499828b2f17fd83d52027
Rahul <pepsi1423@gmail.com>
2017-09-25 23:02:45 -0400

    Added more functionality, started move using dicts

--- Programming Assignment 1/program1.py
+++ Programming Assignment 1/program1.py
@@ -1,7 +1,11 @@
 import sys
 
-board = [[0 for x in range(3)] for y in range(3)]
+board = [[0 for x in range(3)] for y in range(3)] #array for representing the board
+maxNodes = 100000 #max nodes allowed during search, stop when limit is reached
 
+'''
+Sets the current state to be whatever is input. Assumes proper format. 
+'''
 def setState(state):
     i = 0
     j = 0
@@ -13,6 +17,8 @@ def setState(state):
            board[i][j] = c
            j += 1
     printState()
+
+
 '''
 Prints out the board in a 3 by 3 table format, like looking at the puzzle itself
 '''
@@ -22,6 +28,19 @@ def printState():
             print(board[x][y], end="")
         print() 
     print()
+
+'''
+Moves the blank spot in the inputted direction and says invalid if move is not valid
+'''
+def move(dir):
+    return {
+        'up': #do stuff in whitespace
+        'down':
+        'left':
+        'right':
+    }
+    print("That is not a valid move. Please submit either up, down, left, or right.")
+    printState()
 
 while True:
     inp = input('Here is a list of functions. Type what function you would like to run!\nsetState\trandomizeState\n')
